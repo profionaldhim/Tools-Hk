effdff26e0808c6932a5e6a5d37071a3bd983c51
profionaldhim <35074237+profionaldhim@users.noreply.github.com>
2018-10-24 21:15:41 +0300

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+rm -rif Tools-Hk.sh
+rm -rif install.sh
+apt install wget -y
+wget https://github.com/profionaldhim/Tools-Hk/blob/master/Tools-Hk.sh
+wget https://github.com/profionaldhim/Tools-Hk/blob/master/install.sh
+chmod +x install.sh Tools-Hk.sh
+
